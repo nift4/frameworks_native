21d752c2b2bf680648ab7ffda8b453c96f86f132
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-10-26 15:38:15 +0800

    SurfaceFlinger: enable console management
    The console management was removed since ICS. But it's useful for debugging.
    Re-implement it based on the original class ConsoleManagerThread of class
    DisplayHardwareBase.

--- services/surfaceflinger/DisplayDevice.h
+++ services/surfaceflinger/DisplayDevice.h
@@ -49,6 +49,7 @@ class HWComposer;
 class IGraphicBufferProducer;
 class Layer;
 class SurfaceFlinger;
+class ConsoleManagerThread;
 
 struct CompositionInfo;
 struct DisplayDeviceCreationArgs;
@@ -184,6 +185,10 @@ private:
     Vector< sp<Layer> > mVisibleLayersSortedByZ;
     // list of layers needing fences
     Vector< sp<Layer> > mLayersNeedingFences;
+
+#ifdef CONSOLE_MANAGER
+    sp<ConsoleManagerThread> mConsoleManagerThread;
+#endif
 
     /*
      * Transaction state
