ff7c25862b223b009df9e20b4d48a84c96454241
Jon Doe <tuksgig@gmail.com>
2018-12-23 22:50:00 +0800

    inputflinger: treat tablet-style inputs as absolute coordinate mouse pointer
    Qemu and VirtualBox use tablet-style inputs. However, it's difficult to
    work with the current "invisible finger" or "drag pointer" interface
    provided to the virtual absolute coordinate pointing devices.
    Instead, this patch classifies them as a regular mouse pointer
    (INPUT_DEVICE_CLASS_CURSOR), which is more intuitive to work with.

--- services/inputflinger/InputReader.h
+++ services/inputflinger/InputReader.h
@@ -677,10 +677,16 @@ public:
 
     inline int32_t getRelativeX() const { return mRelX; }
     inline int32_t getRelativeY() const { return mRelY; }
+    inline int32_t getAbsoluteX() const { return mAbsX; }
+    inline int32_t getAbsoluteY() const { return mAbsY; }
+    inline bool hasMoved() const { return mMoved; }
 
 private:
     int32_t mRelX;
     int32_t mRelY;
+    int32_t mAbsX;
+    int32_t mAbsY;
+    bool mMoved;
 
     void clearRelativeAxes();
 };
@@ -1176,6 +1182,7 @@ private:
         Mode mode;
         bool hasAssociatedDisplay;
         bool orientationAware;
+        bool hasAbsAxis;
     } mParameters;
 
     CursorButtonAccumulator mCursorButtonAccumulator;
@@ -1183,6 +1190,8 @@ private:
     CursorScrollAccumulator mCursorScrollAccumulator;
 
     int32_t mSource;
+    RawAbsoluteAxisInfo mRawAbsXInfo;
+    RawAbsoluteAxisInfo mRawAbsYInfo;
     float mXScale;
     float mYScale;
     float mXPrecision;
@@ -1207,6 +1216,7 @@ private:
     void configureParameters();
     void dumpParameters(std::string& dump);
 
+    void rotateAbsolute(float* absX, float* absY);
     void sync(nsecs_t when);
 };
 
