nift4/frameworks_native

commit 0c8552ec5ea3b20285f5c221f93ff92776d03b11

libEGL: select pixel format by EGL_NATIVE_VISUAL_ID
We need this workaround until Mesa supports HAL_PIXEL_FORMAT_RGBA_8888.

opengl/libs/EGL/eglApi.cpp
@@ -657,6 +657,9 @@ void getNativePixelFormat(EGLDisplay dpy, egl_connection_t* cnx, EGLConfig confi
         if (componentType == EGL_COLOR_COMPONENT_TYPE_FIXED_EXT) {
             if (colorDepth > 24) {
                 *format = HAL_PIXEL_FORMAT_RGBA_1010102;
+            } else if (cnx->egl.eglGetConfigAttrib(dpy, config, EGL_NATIVE_VISUAL_ID, &colorDepth)) {
+                ALOGE("eglGetConfigAttrib(EGL_NATIVE_VISUAL_ID) failed: %#x", eglGetError());
+                *format = android_pixel_format(colorDepth);
             } else {
                 *format = HAL_PIXEL_FORMAT_RGBA_8888;
             }

commit 98466f3591804e015b7ce376fd6f0e03034f219e

libEGL: remove incorrect error message
The logic in commit 0c8552ec5 is reversed.

Fixes: 0c8552ec5 ("libEGL: select pixel format by EGL_NATIVE_VISUAL_ID")
Reported-by: Michael Goffioul <michael.goffioul@gmail.com>

opengl/libs/EGL/eglApi.cpp
@@ -658,9 +658,9 @@ void getNativePixelFormat(EGLDisplay dpy, egl_connection_t* cnx, EGLConfig confi
             if (colorDepth > 24) {
                 *format = HAL_PIXEL_FORMAT_RGBA_1010102;
             } else if (cnx->egl.eglGetConfigAttrib(dpy, config, EGL_NATIVE_VISUAL_ID, &colorDepth)) {
-                ALOGE("eglGetConfigAttrib(EGL_NATIVE_VISUAL_ID) failed: %#x", eglGetError());
                 *format = android_pixel_format(colorDepth);
             } else {
+                ALOGE("eglGetConfigAttrib(EGL_NATIVE_VISUAL_ID) failed: %#x", eglGetError());
                 *format = HAL_PIXEL_FORMAT_RGBA_8888;
             }
         } else {