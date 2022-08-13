c84733b9edc82d832c4af91777c4d156a07840e5
nift4 <nift4@protonmail.com>
2022-08-13 12:26:35 +0200

    inputflinger: add absolute mouse input support
    Note: This is the backport for Android 12. Please use the latest version
          of the patch (without this message) for Android 13 and up!
    
    Squashed:
    commit 3f067c70d99ee46f158afee4b926d2cfac56d9fa
    Author: nift4 <nift4@protonmail.com>
    Date:   Mon May 8 19:05:36 2023 +0200
    
        fixup! inputflinger: add absolute mouse input support
    
        Rewritten version of patch partially based on patch by jdlouw
    
        Change-Id: Idb7805178b4f7ad59100f0de89b72d7a98033849
    
    Change-Id: I3437065e04830cf225a496ed0fb51f8c4544e58e

--- services/inputflinger/reader/EventHub.cpp
+++ services/inputflinger/reader/EventHub.cpp
@@ -165,6 +165,13 @@ uint32_t getAbsAxisUsage(int32_t axis, uint32_t deviceClasses) {
             case ABS_MT_PRESSURE:
             case ABS_MT_DISTANCE:
                 return INPUT_DEVICE_CLASS_TOUCH;
+
+    // Absolute mouse support
+    if (deviceClasses & INPUT_DEVICE_CLASS_CURSOR)) {
+        switch (axis) {
+            case ABS_X:
+            case ABS_Y:
+                return INPUT_DEVICE_CLASS_CURSOR;
         }
     }
 
@@ -1354,8 +1361,9 @@ status_t EventHub::openDeviceLocked(const char *devicePath, bool ignoreAlreadyOp
     }
 
     // See if this is a cursor device such as a trackball or mouse.
-    if (test_bit(BTN_MOUSE, device->keyBitmask) && test_bit(REL_X, device->relBitmask) &&
-        test_bit(REL_Y, device->relBitmask)) {
+    if (test_bit(BTN_MOUSE, device->keyBitmask) && ((test_bit(REL_X, device->relBitmask) &&
+        test_bit(REL_Y, device->relBitmask)) || (test_bit(ABS_X, device->relBitmask) &&
+        test_bit(ABS_Y, device->relBitmask) && test_bit(REL_WHEEL, device->relBitmask)))) {
         device->classes |= INPUT_DEVICE_CLASS_CURSOR;
     }
 
@@ -1380,7 +1388,7 @@ status_t EventHub::openDeviceLocked(const char *devicePath, bool ignoreAlreadyOp
         }
         // Is this an old style single-touch driver?
     } else if ((test_bit(BTN_TOUCH, device->keyBitmask) || test_bit(BTN_LEFT, device->keyBitmask)) && test_bit(ABS_X, device->absBitmask) &&
-               test_bit(ABS_Y, device->absBitmask)) {
+               test_bit(ABS_Y, device->absBitmask) && !test_bit(REL_WHEEL, device->relBitmask)) {
         device->classes |= INPUT_DEVICE_CLASS_TOUCH;
         // Is this a BT stylus?
     } else if ((test_bit(ABS_PRESSURE, device->absBitmask) ||
